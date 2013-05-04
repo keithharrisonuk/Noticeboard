407531f3d33b43be1aa369ee8c671e67bf7d94eb
Keith Harrison <keithharrisonuk@yahoo.co.uk>
2013-05-04 13:54:27 +0100

    Add files

--- Noticeboard/urls.py
+++ Noticeboard/urls.py
@@ -6,8 +6,8 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-                       url(r'^NoticeboardApp/', include('NoticeboardApp.urls')),
-                       # Uncomment the next line to enable the admin:
                        url(r'^admin/', include(admin.site.urls)),
+                       url(r'^NoticeboardApp/', include('NoticeboardApp.urls')),
+                       url(r'^$', 'NoticeboardApp.views.index'),
                        )
 
