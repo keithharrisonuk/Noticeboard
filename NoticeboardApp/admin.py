b60dc49302709a10339bc4e3ccc30a0d26f34a07
Keith Harrison <keithharrisonuk@yahoo.co.uk>
2013-05-04 22:00:40 +0100

    Add files

---
+++ NoticeboardApp/admin.py
@@ -0,0 +1,6 @@
+from NoticeboardApp.models import Category, JobType, Notice
+from django.contrib import admin
+
+admin.site.register(Category)
+admin.site.register(JobType)
+admin.site.register(Notice)
--- NoticeboardApp/models.py
+++ NoticeboardApp/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __unicode__(self):
+        return self.name
+
+
+class JobType(models.Model):
+    Category = models.ForeignKey(Category)
+    name = models.CharField(max_length=50)
+
+    def __unicode__(self):
+        return self.name
+
+class Notice(models.Model):
+    JobType = models.ForeignKey(JobType)
+    CompanyName = models.CharField(max_length=100)
+    FirstName = models.CharField(max_length=30)
+    LastName = models.CharField(max_length=30)
+    EmailAddress = models.CharField(max_length=150)
+    Mobile = models.CharField(max_length=15)
+    LandLine = models.CharField(max_length=15)
+    def __unicode__(self):
+        return self.JobType.name + " " + self.CompanyName + " " + self.FirstName + " "+ self.LastName
+
+
+
