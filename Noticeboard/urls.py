62a882b42979c9f8e3c16bbf89b78f73a9f4e845
Keith Harrison <keithharrisonuk@yahoo.co.uk>
2013-05-07 23:02:26 +0100

    Add files

--- Noticeboard/urls.py
+++ Noticeboard/urls.py
@@ -8,11 +8,11 @@ admin.autodiscover()
 urlpatterns = patterns('',
                        url(r'^admin/', include(admin.site.urls)),
                        url(r'^NoticeboardApp/', include('NoticeboardApp.urls')),
-                       #url(r'^$', RedirectView.as_view(url='NoticeboardApp')),
+                       url(r'^$', RedirectView.as_view(url='NoticeboardApp')),
                        )
 
 # Serve static files when debug false
-#if not settings.DEBUG:
-urlpatterns += patterns('',
-    (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
-)
+if not settings.DEBUG:
+    urlpatterns += patterns('',
+        (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
+    )
