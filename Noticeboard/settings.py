6c31b70083e40b9cc6e7495761f279caed5f3e7f
Keith Harrison <keithharrisonuk@yahoo.co.uk>
2013-05-07 23:34:27 +0100

    Add files

--- Noticeboard/settings.py
+++ Noticeboard/settings.py
@@ -2,7 +2,7 @@
 import dj_database_url
 import os
 
-DEBUG = os.environ.get('DEBUG', False)
+DEBUG = bool(os.environ.get('DEBUG', False))
 TEMPLATE_DEBUG = DEBUG
 
 ADMINS = (
@@ -29,7 +29,7 @@ DATABASES = {
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'http://keithsawsomenoticeboard.herokuapp.com/']
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
