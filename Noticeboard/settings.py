a75e22b1e5aaabbeb8463890743578f45382ece6
Keith Harrison <keithharrisonuk@yahoo.co.uk>
2013-05-07 22:43:38 +0100

    Add files

--- Noticeboard/settings.py
+++ Noticeboard/settings.py
@@ -12,7 +12,8 @@ ADMINS = (
 MANAGERS = ADMINS
 
 PROJECT_DIR = os.path.dirname(__file__)
-
+PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
+PROJECT_DIR = os.path.join(PROJECT_ROOT,'../NoticeboardApp')
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
@@ -66,18 +67,11 @@ MEDIA_URL = ''
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
 PROJECT_DIR = os.path.dirname(__file__)
-STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
 
-# URL prefix for static files.
-# Example: "http://example.com/static/", "http://static.example.com/"
+STATIC_ROOT = os.path.join(PROJECT_ROOT,'static/')
 STATIC_URL = '/static/'
-
-# Additional locations of static files
 STATICFILES_DIRS = (
-    # Put strings here, like "/home/html/static" or "C:/www/django/static".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
-    os.path.join(os.path.abspath(os.path.dirname(__file__) + '/..'), 'static'),
+    os.path.join(PROJECT_DIR,'static/'),
 )
 
 # List of finder classes that know how to find static files in
